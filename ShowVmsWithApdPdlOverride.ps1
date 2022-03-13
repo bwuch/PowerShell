4f7846f9da3ab7c54ab73f742f18f4692a5544b1
Brian Wuchner <brian.wuchner@gmail.com>
2022-03-13 15:27:31 -0400

    Update ShowVmsWithApdPdlOverride.ps1

--- ShowVmsWithApdPdlOverride.ps1
+++ ShowVmsWithApdPdlOverride.ps1
@@ -1,3 +1,8 @@
 $vmHash = Get-VM | Select-Object ID, Name, @{N='Cluster';E={$_.VMHost.Parent}} | Group-Object ID -AsHashTable
 
-(Get-Cluster).ExtensionData.ConfigurationEx.DasVmConfig | Select-Object @{N='Cluster';E={ $vmHash["$($_.Key)"].Cluster }}, @{N='Name';E={ $vmHash["$($_.Key)"].Name }}, @{N='VmStorageProtectionForAPD';E={$_.DasSettings.VmComponentProtectionSettings.VmStorageProtectionForAPD}}, @{N='VmStorageProtectionForPDL';E={$_.DasSettings.VmComponentProtectionSettings.VmStorageProtectionForPDL}}
+(Get-Cluster).ExtensionData.Configuration.DasVmConfig | Select-Object @{N='Cluster';E={ $vmHash["$($_.Key)"].Cluster }}, @{N='Name';E={ $vmHash["$($_.Key)"].Name }}, 
+@{N='VmRestartPriority';E={$_.DasSettings.RestartPriority}}, 
+@{N='VmStorageProtectionForPDL';E={$_.DasSettings.VmComponentProtectionSettings.VmStorageProtectionForPDL}},
+@{N='VmStorageProtectionForAPD';E={$_.DasSettings.VmComponentProtectionSettings.VmStorageProtectionForAPD}}, 
+@{N='ApdVmFailoverDelaySeconds';E={$_.DasSettings.VmComponentProtectionSettings.VmTerminateDelayForAPDSec}}, 
+@{N='ApdResposeRecovery';E={$_.DasSettings.VmComponentProtectionSettings.VmReactionOnAPDCleared}}
